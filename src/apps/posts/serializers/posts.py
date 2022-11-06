6371834b3101f26781e04d0184f09d151127d6c8
bekzod-fayzikuloff <fayzikuloff.b@gmail.com>
2022-11-06 19:37:47 +0300

    :construction: Add base posts endpoints support

---
+++ src/apps/posts/serializers/posts.py
@@ -0,0 +1,41 @@
+from typing import TypeVar
+
+from rest_framework import serializers
+
+from apps.profiles.services import get_profile
+
+from ..models import Post, PostMedia
+
+PostSerializeT = TypeVar("PostSerializeT", bound=serializers.ModelSerializer)
+
+
+class PostMediaCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostMedia
+        fields = "__all__"
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    creator = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ("description", "medias", "is_active", "creator")
+
+    def create(self, validated_data):
+        validated_data["creator"] = get_profile(user=self.context.get("request").user)
+        result = super().create(validated_data)
+        return result
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__"
+        depth = 1
+
+
+class PostUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        exclude = ("creator", "medias")
---
+++ src/apps/posts/views.py
@@ -0,0 +1,80 @@
+from drf_spectacular.utils import extend_schema
+from rest_framework import mixins, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Comment, Post
+from .serializers.comments import CommentCreateSerializer, CommentSerializerT, CommentUpdateSerializer
+from .serializers.posts import (
+    PostCreateSerializer,
+    PostMediaCreateSerializer,
+    PostSerializer,
+    PostSerializeT,
+    PostUpdateSerializer,
+)
+
+
+class CommentViewSet(
+    mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet
+):
+    queryset = Comment.objects.all()
+    serializer_class = CommentCreateSerializer
+
+    @extend_schema(
+        methods=["POST"], request=CommentCreateSerializer, responses={status.HTTP_201_CREATED: CommentCreateSerializer}
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @extend_schema(
+        methods=["PUT"], request=CommentUpdateSerializer, responses={status.HTTP_200_OK: CommentCreateSerializer}
+    )
+    def update(self, request, *args, **kwargs) -> Response:
+        return super().update(request, *args, **kwargs)
+
+    @extend_schema(
+        methods=["PATCH"], request=CommentUpdateSerializer, responses={status.HTTP_200_OK: CommentCreateSerializer}
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @extend_schema()
+    def destroy(self, request, *args, **kwargs) -> Response:
+        return super().destroy(request, *args, **kwargs)
+
+    def get_serializer_class(self) -> type[CommentSerializerT]:
+        match self.action:
+            case "create":
+                return CommentCreateSerializer
+            case "update":
+                return CommentUpdateSerializer
+            case "partial_update":
+                return CommentUpdateSerializer
+            case _:
+                return CommentCreateSerializer
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    @action(methods=["POST"], detail=False, url_path=r"medias")
+    def create_post_media(self, request: Request) -> Response:
+        return super().create(request)
+
+    def get_serializer_class(self) -> type[PostSerializeT]:
+        match self.action:
+            case "create":
+                return PostCreateSerializer
+            case "update":
+                return PostUpdateSerializer
+            case "partial_update":
+                return PostUpdateSerializer
+            case "create_post_media":
+                return PostMediaCreateSerializer
+            case _:
+                return PostSerializer
