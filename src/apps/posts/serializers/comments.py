b78dfce1356a0e78754684a9f1dfaaf19609fee2
fayzikuloff.b <fayzikuloff.b@gmail.com>
2023-04-15 00:17:32 +0500

    Simple change comment create serialzer

--- src/apps/posts/serializers/comments.py
+++ src/apps/posts/serializers/comments.py
@@ -34,10 +34,36 @@ class CommentListSerializer(serializers.ModelSerializer):
 
 class CommentCreateSerializer(serializers.ModelSerializer):
     creator = serializers.PrimaryKeyRelatedField(read_only=True)
+    creator_id = serializers.SerializerMethodField()
+    avatar = serializers.SerializerMethodField()
+    username = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_username(instance: Comment):
+        return CommentListSerializer.get_username(instance)
+
+    @staticmethod
+    def get_avatar(instance: Comment):
+        return CommentListSerializer.get_avatar(instance)
+
+    @staticmethod
+    def get_creator_id(instance: Comment):
+        return CommentListSerializer.get_creator_id(instance)
 
     class Meta:
         model = Comment
-        fields = ("id", "creator", "post", "content", "answer")
+        fields = (
+            "id",
+            "creator",
+            "post",
+            "content",
+            "answer",
+            "created_at",
+            "updated_at",
+            "creator_id",
+            "avatar",
+            "username",
+        )
 
     def create(self, validated_data):
         validated_data["creator"] = get_profile(user=self.context.get("request").user)
